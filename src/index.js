0c7ed65e47bffecf32fac92ce3ac8b0ca1f25088
Michael Tamirie <michaeltamirie@gmail.com>
2022-02-17 00:16:58 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -6,27 +6,27 @@ let template = '';
 let tick = '';
 let display = '';
 const taskLists = [
-    {
-        description: 'Wash the dishes',
-        completed: false,
-        index: 0,
-    },
-    {
-        description: 'Complete To do list project',
-        completed: false,
-        index: 1,
-    },
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Complete To do list project',
+    completed: false,
+    index: 1,
+  },
 ];
 
 for (let i = 0; i < taskLists.length; i += 1) {
-    if (taskLists[i].completed === false) {
-        tick = 'none';
-        display = '';
-    } else {
-        tick = '';
-        display = 'none';
-    }
-    template += `
+  if (taskLists[i].completed === false) {
+    tick = 'none';
+    display = '';
+  } else {
+    tick = '';
+    display = 'none';
+  }
+  template += `
     <div class="task" id="${taskLists[i].index}">
         <div class="check_box ${tick}"></div>
         <input type = "checkbox" class="checking ${display}">
